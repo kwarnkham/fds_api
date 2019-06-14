e2fb16817412a649e85bab3f31a8e22fcc0ba214
kwarnkham <kwarnkham.moon@gmail.com>
2019-06-14 16:09:54 +0630

    parsing pics

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,10 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Http\File;
 
 class ProductController extends Controller
 {
     public function store(Request $request){
-        return $request;
+        //1. validate
+        //2. store product
+        //3. store pics from the stored prodcut
+        //4. return the product info
+        
+        // $files = $request->files;
+        // return $request->files;
+        $data= [];
+        foreach($request->file('files') as $img){
+            $path=Storage::putFile('public/product_pictures', new File($img)); //stored in storage
+            array_push($data, $path);
+        }
+        return $data;
+        // return $request->files;
     }
 }
