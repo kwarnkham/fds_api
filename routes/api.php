90dbc3b9efea368c2eae7d4cd67a6dbc243cd986
kwarnkham <kwarnkham.moon@gmail.com>
2019-06-24 16:22:55 +0630

    remove closure in api for route cache

--- routes/api.php
+++ routes/api.php
@@ -13,9 +13,9 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 // Route::post('/user/create', 'UserController@store');
 // Route::post('/api_token/create', 'ApiTokenController@store');
